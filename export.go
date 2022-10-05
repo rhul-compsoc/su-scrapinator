bcdb1681502f94d0c4ab7d7067c30f8e0c7bdcac
djpiper28 <djpiper28@gmail.com>
2022-10-05 11:30:01 +0100

    feat: added mail list exporter

---
+++ export.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"log"
+)
+
+type InputJson struct {
+	Ids []string `json:"ids"`
+}
+
+func main() {
+	var data []byte 
+  data, err := os.ReadFile("members.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var iJson InputJson
+	err = json.Unmarshal(data, &iJson)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  log.Printf("Found %d entries", len(iJson.Ids))
+
+	for i := 0; i < len(iJson.Ids); i++ {
+		fmt.Printf("%s@live.rhul.ac.uk,", iJson.Ids[i])
+	}
+}
